27c70932c78c8b110c5191c6cdb78537b0f65836
lukasmisiukevicius <misiukeviciuslukas@yahoo.com>
2022-10-23 16:19:47 +0300

    Almost Finished

--- app/src/main/java/com/example/lab3/MainActivity.java
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -1,18 +1,20 @@
 package com.example.lab3;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.android.material.button.MaterialButton;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     TextView tvCalc, tvResult;
     Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnDot, btnPlus, btnMinus, btnMultiply, btnDivide, btnEqual, btnC;
+    Button btnCE, btnPlusminus, btnRoot, btnBackspace;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +39,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         assignId(btnDivide,R.id.btnDivide);
         assignId(btnEqual,R.id.btnEqual);
         assignId(btnC,R.id.btnC);
+        assignId(btnCE,R.id.btnCE);
+        assignId(btnPlusminus,R.id.btnPlusMinus);
+        assignId(btnRoot,R.id.btnRootSqr);
+        assignId(btnBackspace,R.id.btnBackSpace);
     }
 
     void assignId(Button btn, int id){
@@ -50,14 +56,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String buttonText = button.getText().toString();
         String dataToCalculate = tvCalc.getText().toString();
 
-        if(buttonText.equals("C"))
+        if(buttonText.equals("CE") || buttonText.equals("C"))
         {
             tvCalc.setText("");
             tvResult.setText("0");
             return;
-
         }
-        dataToCalculate = dataToCalculate+buttonText;
+        if(buttonText.equals("="))
+        {
+            if(dataToCalculate.contains("√"))
+            {
+                String first[] = dataToCalculate.split("√");
+
+                tvResult.setText(String.valueOf(Math.pow(Double.parseDouble(first[0]),1.0/2)));
+
+            }else{
+                tvCalc.setText(tvResult.getText().toString());
+            }
+            return;
+        }
+        if(buttonText.equals("←"))
+        {
+            dataToCalculate = dataToCalculate.substring(0,dataToCalculate.length()-1);
+        }else
+        {
+            dataToCalculate = dataToCalculate+buttonText;
+        }
+
         tvCalc.setText(dataToCalculate);
+
+        String finalResult = getResult(dataToCalculate);
+        if(!finalResult.equals("Error"))
+        {
+            tvResult.setText(finalResult);
+        }
+    }
+    String getResult(String data)
+    {
+
+        try {
+            Context context = Context.enter();
+            context.setOptimizationLevel(-1);
+            Scriptable scriptable = context.initStandardObjects();
+            String finalResult = context.evaluateString(scriptable, data, "Javascript", 1, null).toString();
+            if(finalResult.endsWith(".0")){
+                finalResult = finalResult.replace(".0","");
+            }
+            return finalResult;
+        }catch (Exception e){
+            return "Error";
+        }
     }
 }
